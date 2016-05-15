7c7fcfb63c5fe5097b680118144ae4aabd08bfdd
Col Chhina <colchhina@Cols-MBP-3.att.net>
2016-05-14 18:31:57 -0700

    Setup & styled basic project functionality

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160511224246) do
+ActiveRecord::Schema.define(version: 20160514211016) do
 
   create_table "posts", force: :cascade do |t|
     t.string   "title"
     t.text     "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "projects", force: :cascade do |t|
+    t.string   "title"
+    t.string   "description"
+    t.string   "link"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
   end
 
 end
